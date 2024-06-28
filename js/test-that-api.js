c7e792c0f83c092215f8c9ca2e7df1b90a8b9e5a
demuelle <dmueller@trilogyed.com>
2024-06-28 10:40:29 -0500

    Adds output to main page

--- js/test-that-api.js
+++ js/test-that-api.js
@@ -1,14 +1,31 @@
 console.log("ok");
 const input = document.querySelector("#input-url");
+const urlSpan = document.querySelector("#url-display");
+const statusDiv = document.querySelector("#status");
+const outputDiv = document.querySelector("#output");
+const errorDiv = document.querySelector("#error");
 
-document.querySelector("#try-it").addEventListener("click", function(event) {
+document.querySelector("#try-it").addEventListener("click", function (event) {
   event.preventDefault();
   const url = input.value.trim();
-  fetch(url).then(response => response.json()).then(
-    function(data) {
-      return console.log(data)
-    }
-  ).catch(err => console.error(err));
+  urlSpan.textContent = url;
+  statusDiv.textContent = "";
+  outputDiv.textContent = "";
+  errorDiv.textContetn = "";
+  fetch(url).then(
+    function (response) {
+      statusDiv.textContent = response.status;
+      console.log(response);
+      return response.json();
+    }).then(
+      function (data) {
+        outputDiv.textContent = JSON.stringify(data, null, 3);
+        return console.log(data)
+      }
+    ).catch(err => {
+      errorDiv.textContent = err;
+      console.error(err)
+    });
   console.log("woohoo");
 })
 
